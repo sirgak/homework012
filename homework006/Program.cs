32dca6983b0bdb653740847613deee81e2567751
sirgak <life_4_ever@mail.ru>
2022-05-23 09:36:41 +0300

    test
    test

---
+++ homework006/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grandmasAmount;
+            int onePersonWaitingTime = 10;
+            int fullTime;
+            int hour;
+            int min;
+            Console.Write("Введите количество бабушек: ");
+            grandmasAmount =Convert.ToInt32 (Console.ReadLine());
+            fullTime = grandmasAmount * onePersonWaitingTime;
+            hour = fullTime / 60;
+            min = fullTime % 60;
+            Console.WriteLine("Вы должны отстоять в очереди " + hour + " часа " + min + "минут.");
+        }
+    }
+}
